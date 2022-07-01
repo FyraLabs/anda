7487a02e2505e8e2662fe49b3a5457fc98041ca7
windowsboy111 <wboy111@outlook.com>
2022-07-01 10:50:09 +0800

    trying chain

--- rpm-parse/src/main.rs
+++ rpm-parse/src/main.rs
@@ -1,24 +1,20 @@
+use libflate::gzip::Decoder;
 use quick_xml::{
     events::{attributes::Attribute, BytesStart, Event},
     Reader,
 };
-use reqwest;
+use reqwest::blocking::Response;
 use std::{
-    borrow::BorrowMut,
     borrow::Cow,
     error::Error,
-    fs::File,
-    io::{self, BufReader, Write},
+    io::BufReader,
     str,
     string::FromUtf8Error,
     time::SystemTime,
 };
-use tokio;
 
 const URL: &str = "https://ftp.yz.yamagata-u.ac.jp/pub/linux/fedora-projects/fedora/linux/releases/36/Everything/x86_64/os/repodata/f0f7fc0fca36fdec9d2b684d7fd8a07512e49d74849acfbb2a74a96989b6f180-filelists.xml.gz";
-const FILE: &str = "filelists.xml.gz";
-const SIZE: f32 = 53282995.;
-const FILE2: &str = "filelists.xml";
+type Rd = Reader<BufReader<Decoder<Response>>>;
 
 struct Pkg {
     id: String,
@@ -42,42 +38,27 @@ fn get_attr(e: &BytesStart, attrname: &str) -> Result<String, Box<dyn Error>> {
     }
 }
 
-fn goto_next_pkg(reader: &mut Reader<BufReader<File>>) {
+fn goto_next_pkg(reader: &mut Rd) {
     let mut buf = vec![];
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::End(e)) => {
-                if String::from_utf8_lossy(e.name()) == "package" {
-                    break;
-                }
-            }
-            Err(e) => {
-                panic!("Error at position {}: {:?}", reader.buffer_position(), e)
-            }
+            Ok(Event::End(_)) => break,
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (),
         }
     }
 }
 
-fn parse_pkg(reader: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<Pkg, Box<dyn Error>> {
+fn parse_pkg(reader: &mut Rd, e: &BytesStart) -> Result<Pkg, Box<dyn Error>> {
+    let mut buf = vec![];
     let mut pkg = Pkg {
-        id: String::new(),
-        name: String::new(),
-        arch: String::new(),
+        id: get_attr(&e, "pkgid")?,
+        name: get_attr(&e, "name")?,
+        arch: get_attr(&e, "arch")?,
         ver: String::new(),
         rel: String::new(),
         files: vec![],
     };
-    for attr in e.attributes() {
-        let attr = attr?;
-        match attr.key {
-            b"pkgid" => pkg.id = ex_attr(attr)?,
-            b"name" => pkg.name = ex_attr(attr)?,
-            b"arch" => pkg.arch = ex_attr(attr)?,
-            _ => (),
-        }
-    }
-    let mut buf = vec![];
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Empty(e)) => {
@@ -113,56 +94,12 @@ fn parse_pkg(reader: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<Pkg
     Ok(pkg)
 }
 
-async fn download() -> Result<Vec<u8>, Box<dyn Error>> {
-    println!("Downloading...");
-    let mut response = reqwest::get(URL).await?;
-    let mut file = File::create(FILE)?;
-    let mut size: usize = 0;
-    let time = SystemTime::now();
-    let mut gz: Vec<u8> = Vec::new();
-    while let Ok(Some(mut item)) = response.chunk().await {
-        size += item.len();
-        print!(
-            "{}\r {:.2}% | {:.0} KiB/s | {}/{}",
-            format!("{}[2K", 27 as char),
-            size as f32 / SIZE * 100.,
-            size as f32
-                / 1024.
-                / SystemTime::now()
-                    .duration_since(time)
-                    ?
-                    .as_secs_f32(),
-            size,
-            SIZE as i32
-        );
-        io::stdout().flush()?;
-        file.write_all(item.borrow_mut())?;
-        gz.append(&mut item.to_vec());
-    }
-    file.sync_all()?;
-    Ok(gz)
-}
 
-fn decompress(gz: Vec<u8>) -> Result<(), Box<dyn Error>> {
-    println!("\nDecompressing...");
-    let time = SystemTime::now();
-    let mut decompressed = File::create(FILE2)?;
-    let mut inflate = libflate::gzip::Decoder::new(&*gz)?;
-    io::copy(&mut inflate, &mut decompressed)?;
-    println!(
-        "Decompression took {} seconds",
-        SystemTime::now().duration_since(time)?.as_secs()
-    );
-    decompressed.sync_all()?;
-    Ok(())
-}
-
-fn parse() -> Result<(), Box<dyn Error>> {
-    println!("Parsing XML...");
-    let mut reader = quick_xml::Reader::from_file(FILE2)?;
+fn parse(mut reader: Rd) -> Result<(), Box<dyn Error>> {
+    println!("Parsing");
     let mut buf: Vec<u8> = Vec::new();
-    let t = SystemTime::now();
     let mut pkgs: Vec<Pkg> = Vec::new();
+    let t = SystemTime::now();
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Start(ref e)) => {
@@ -171,9 +108,13 @@ fn parse() -> Result<(), Box<dyn Error>> {
                     goto_next_pkg(&mut reader);
                     continue;
                 }
+                if get_attr(&e, "name")? != "0ad-data" {
+                    println!("{}", get_attr(&e, "name")?);
+                    goto_next_pkg(&mut reader);
+                    continue;
+                }
                 pkgs.push(parse_pkg(&mut reader, e)?);
             }
-            Ok(Event::Text(_)) => (), // panic!("Didn't expect text at {}", reader.buffer_position()),
             Ok(Event::Eof) => break,
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (),
@@ -187,11 +128,11 @@ fn parse() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // let gz = download().await?;
-    // decompress(gz)?;
-    parse()?;
-
+fn main() -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::blocking::get(URL)?;
+    let decoder = Decoder::new(resp)?;
+    let bufreader = BufReader::new(decoder);
+    let reader = Reader::from_reader(bufreader);
+    parse(reader)?;
     Ok(())
 }
