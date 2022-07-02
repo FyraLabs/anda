b9dbef6df76393084668d2fdac95f5f8ba08fcb7
windowsboy111 <wboy111@outlook.com>
2022-07-02 14:37:14 +0800

    chain decompress and parsing

--- rpm-parse/src/main.rs
+++ rpm-parse/src/main.rs
@@ -1,20 +1,13 @@
 use libflate::gzip::Decoder;
 use quick_xml::{
-    events::{attributes::Attribute, BytesStart, Event},
+    events::{BytesStart, Event},
     Reader,
 };
-use reqwest::blocking::Response;
-use std::{
-    borrow::Cow,
-    error::Error,
-    io::BufReader,
-    str,
-    string::FromUtf8Error,
-    time::SystemTime,
-};
+use std::{error::Error, io::BufReader, str, time::{SystemTime, Duration}};
 
 const URL: &str = "https://ftp.yz.yamagata-u.ac.jp/pub/linux/fedora-projects/fedora/linux/releases/36/Everything/x86_64/os/repodata/f0f7fc0fca36fdec9d2b684d7fd8a07512e49d74849acfbb2a74a96989b6f180-filelists.xml.gz";
-type Rd = Reader<BufReader<Decoder<Response>>>;
+type Rd<'a> = Reader<BufReader<Decoder<&'a [u8]>>>;
+type Res<T> = Result<T, Box<dyn Error>>;
 
 struct Pkg {
     id: String,
@@ -25,31 +18,25 @@ struct Pkg {
     files: Vec<String>, // /usr/share/pkgname/...
 }
 
-fn extract_cow(c: Cow<[u8]>) -> Result<String, FromUtf8Error> {
-    Ok(format!("{}", String::from_utf8(c.to_vec())?))
-}
-fn ex_attr(a: Attribute) -> Result<String, FromUtf8Error> {
-    extract_cow(a.value)
-}
-fn get_attr(e: &BytesStart, attrname: &str) -> Result<String, Box<dyn Error>> {
+fn get_attr(e: &BytesStart, attrname: &str) -> Res<String> {
     match e.try_get_attribute(attrname)? {
-        Some(attr) => Ok(ex_attr(attr)?),
-        None => panic!("Expect value for {}", attrname),
+        Some(attr) => Ok(format!("{}", String::from_utf8(attr.value.to_vec())?)),
+        None => panic!("Expect value for {} on tag {:?}", attrname, e),
     }
 }
 
-fn goto_next_pkg(reader: &mut Rd) {
+fn goto_next_pkg(mut reader: Rd) -> Rd {
     let mut buf = vec![];
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::End(_)) => break,
+            Ok(Event::End(_)) => return reader,
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (),
         }
     }
 }
 
-fn parse_pkg(reader: &mut Rd, e: &BytesStart) -> Result<Pkg, Box<dyn Error>> {
+fn parse_pkg(reader: &mut Rd, e: BytesStart) -> Res<Pkg> {
     let mut buf = vec![];
     let mut pkg = Pkg {
         id: get_attr(&e, "pkgid")?,
@@ -94,23 +81,39 @@ fn parse_pkg(reader: &mut Rd, e: &BytesStart) -> Result<Pkg, Box<dyn Error>> {
     Ok(pkg)
 }
 
-
-fn parse(mut reader: Rd) -> Result<(), Box<dyn Error>> {
+fn find_pkg(mut reader: Rd, key: &str, value: &str) -> Res<Option<Pkg>> {
     println!("Parsing");
-    let mut buf: Vec<u8> = Vec::new();
+    // let mut waitforend = vec![];
+    loop {
+        match reader.read_event(&mut vec![]) {
+            Ok(Event::Start(e)) => {
+                let tag = String::from_utf8(e.name().to_vec())?;
+                if tag == "package" && get_attr(&e, key)? == value {
+                    return Ok(Some(parse_pkg(&mut reader, e)?));
+                }
+                // waitforend.push(tag);
+            }
+            // Ok(Event::End(ref e)) => {
+            //     assert_eq!(String::from_utf8(e.name().to_vec())?, waitforend.pop().unwrap());
+            // }
+            Err(e) => return Err(Box::new(e)),
+            Ok(Event::Eof) => return Ok(None),
+            _ => (),
+        }
+    }
+}
+
+//TODO drop goto_next_pkg
+fn parse(mut reader: Rd) -> Res<()> {
+    println!("Parsing");
     let mut pkgs: Vec<Pkg> = Vec::new();
     let t = SystemTime::now();
     loop {
-        match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
+        match reader.read_event(&mut vec![]) {
+            Ok(Event::Start(e)) => {
                 let tag = String::from_utf8_lossy(e.name());
                 if tag != "package" {
-                    goto_next_pkg(&mut reader);
-                    continue;
-                }
-                if get_attr(&e, "name")? != "0ad-data" {
-                    println!("{}", get_attr(&e, "name")?);
-                    goto_next_pkg(&mut reader);
+                    reader = goto_next_pkg(reader);
                     continue;
                 }
                 pkgs.push(parse_pkg(&mut reader, e)?);
@@ -128,11 +131,18 @@ fn parse(mut reader: Rd) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let resp = reqwest::blocking::get(URL)?;
-    let decoder = Decoder::new(resp)?;
+fn main() -> Res<()> {
+    let client = reqwest::blocking::ClientBuilder::new().timeout(Duration::from_secs(60*5)).build()?;
+    println!("Sending request");
+    let resp = client.get(URL).send()?;
+    println!("Downloading");
+    let bytes = resp.bytes()?.to_vec();
+    let decoder = Decoder::new(&*bytes)?;
     let bufreader = BufReader::new(decoder);
     let reader = Reader::from_reader(bufreader);
-    parse(reader)?;
+    let t = SystemTime::now();
+    println!("Finding pkg");
+    find_pkg(reader, "name" ,"zzuf")?.unwrap();
+    println!("Took {} seconds", SystemTime::now().duration_since(t)?.as_secs());
     Ok(())
 }
