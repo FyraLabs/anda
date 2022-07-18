8e1516c0c6c7ffe56f62fc0bd047abee15ceb1f3
Cappy Ishihara <cappy@cappuchino.xyz>
2022-07-18 18:37:23 +0700

    implement folder upload, format

--- anda-server/src/artifacts.rs
+++ anda-server/src/artifacts.rs
@@ -1,24 +1,29 @@
 use anyhow::Result;
 use aws_sdk_s3::types::ByteStream;
-use aws_sdk_s3::{Client, Config, Credentials, Region, Endpoint};
+use aws_sdk_s3::{Client, Config, Credentials, Endpoint, Region};
 use lazy_static::lazy_static;
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
 use std::env;
 use std::path::PathBuf;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use walkdir::WalkDir;
 
 lazy_static! {
-    pub static ref CLIENT: Client = Client::from_conf(Config::builder()
-    .credentials_provider(Credentials::new(
-        env::var("S3_ACCESS_KEY").unwrap().as_str(),
-        env::var("S3_SECRET_KEY").unwrap().as_str(),    
-        None,
-        None,
-        "Environment"
-    )).region(Region::new("us-east-1")).endpoint_resolver(Endpoint::immutable(
-        env::var("S3_ENDPOINT").unwrap().parse().unwrap(),
-    )).build());
-
+    pub static ref CLIENT: Client = Client::from_conf(
+        Config::builder()
+            .credentials_provider(Credentials::new(
+                env::var("S3_ACCESS_KEY").unwrap().as_str(),
+                env::var("S3_SECRET_KEY").unwrap().as_str(),
+                None,
+                None,
+                "Environment"
+            ))
+            .region(Region::new("us-east-1"))
+            .endpoint_resolver(Endpoint::immutable(
+                env::var("S3_ENDPOINT").unwrap().parse().unwrap(),
+            ))
+            .build()
+    );
     pub static ref BUCKET: String = env::var("S3_BUCKET").unwrap();
 }
 
@@ -34,10 +39,10 @@ impl S3Artifact {
         })
     }
 
-    pub async fn upload_file(&self, path: &str, file: PathBuf) -> Result<()> {
+    pub async fn upload_file(&self, dest: &str, src: PathBuf) -> Result<()> {
         // convert path to absoluate path
-        let file_path = file.canonicalize()?;
-        println!("Uploading {} to {}", file_path.display(), path);
+        let file_path = src.canonicalize()?;
+        println!("Uploading {} to {}", file_path.display(), dest);
         // Read file from `file` path
         let mut file = File::open(file_path).await?;
 
@@ -48,31 +53,67 @@ impl S3Artifact {
         // Read entire file into `bytes`
         file.read(&mut bytes).await?;
         // upload to S3
-        self.connection.put_object().key(path).body(ByteStream::from(bytes)).bucket(BUCKET.as_str()).send().await?;
+        self.connection
+            .put_object()
+            .key(dest)
+            .body(ByteStream::from(bytes))
+            .bucket(BUCKET.as_str())
+            .send()
+            .await?;
         // self.connection.put_object(path, &bytes).await?;
         Ok(())
     }
 
+    pub async fn upload_folder(&self, dest: &str, src: PathBuf) -> Result<()> {
+        // convert to relative path
+        Ok(for entry in WalkDir::new(&src) {
+            let entry = entry?;
+            if entry.file_type().is_file() {
+                let file_path = entry.into_path();
+                // get file name
+                // let file_name = file_path.file_name().unwrap().to_str().unwrap();
+                let real_path = format!("{}/{}", dest, file_path.strip_prefix(&src).unwrap().display());
+                println!("Uploading {} to {}", file_path.display(), real_path);
+                self.upload_file(&real_path, file_path).await?;
+            }
+        })
+    }
 }
 
 #[cfg(test)]
 mod test_s3 {
-    use rocket::http::hyper::body::Buf;
+    use std::env::current_dir;
 
     use super::*;
 
     #[tokio::test]
     async fn test_s3() {
         let artifact = S3Artifact::new().unwrap();
-        let ls = artifact.connection.list_objects().bucket(BUCKET.as_str()).send().await.unwrap();
+        let ls = artifact
+            .connection
+            .list_objects()
+            .bucket(BUCKET.as_str())
+            .send()
+            .await
+            .unwrap();
         println!("{:#?}", ls);
-        let a = artifact.connection.get_object().key("/Dockerfile").bucket(BUCKET.as_str()).send().await.unwrap();
-        println!("{:?}", std::str::from_utf8(a.body.collect().await.unwrap().chunk()));
     }
 
     #[tokio::test]
     async fn test_s3_upload() {
         let artifact = S3Artifact::new().unwrap();
-        artifact.upload_file("/Dockerfile", PathBuf::from("./Dockerfile")).await.unwrap();
+        artifact
+            .upload_file("/test/cargo.toml", PathBuf::from("./Cargo.toml"))
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_s3_upload_folder() {
+        let artifact = S3Artifact::new().unwrap();
+        artifact
+            .upload_folder("/test", current_dir().unwrap())
+            .await
+            .unwrap();
     }
 }
