ba782a4e01ac1a71c5130de643351f1ccbd2c14b
Lleyton Gray <lleyton@fyralabs.com>
2022-07-24 17:47:11 -0700

    bootstrap next and nextui

---
+++ anda-frontend/pages/_app.tsx
@@ -0,0 +1,29 @@
+import { AppProps } from "next/app";
+import "../styles/globals.scss";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const lightTheme = createTheme({
+  type: "light",
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <NextThemesProvider
+    defaultTheme="system"
+    attribute="class"
+    value={{
+      light: lightTheme.className,
+      dark: darkTheme.className,
+    }}
+  >
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  </NextThemesProvider>
+);
+
+export default MyApp;
