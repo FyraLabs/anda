a97c70ee90068751b59d6d2da4b3cc81575e6a4c
Lleyton Gray <lleyton@fyralabs.com>
2022-07-25 03:32:29 -0700

    user page stuff

--- anda-frontend/src/pages/User.tsx
+++ anda-frontend/src/pages/User.tsx
@@ -1,4 +1,8 @@
-import { faDocker } from "@fortawesome/free-brands-svg-icons";
+import {
+  faDocker,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import {
   faBox,
   faArrowDown,
@@ -9,15 +13,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const User = () => {
   return (
-    <div className="p-5">
-      <div className="flex gap-3 items-center">
-        <div className="rounded-full bg-teal-500 h-10 w-10"></div>
-        <h1 className="text-3xl font-medium text-gray-200">lleyton</h1>
+    <div className="p-5 dark:text-gray-300">
+      <div className="flex flex-row items-center gap-5">
+        <div className="rounded-full bg-teal-500 h-16 w-16"></div>
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-medium text-gray-200">lleyton</h1>
 
-        <div></div>
+          <p>
+            Hi! My name is Lleyton Gray. I'm a developer from Los Angeles
+            working on packaging for tauOS.
+          </p>
+
+          <div className="flex text-sm gap-2">
+            <div>
+              <FontAwesomeIcon icon={faTwitter} fixedWidth className="mr-1" />
+              @lleyton__
+            </div>
+
+            <div>
+              <FontAwesomeIcon icon={faGithub} fixedWidth className="mr-1" />
+              @lleyton
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="flex divide-y-[1px] divide-neutral-800 flex-col dark:text-gray-300">
+      <div className="flex divide-y-[1px] divide-neutral-800 flex-col">
         <div className="flex gap-5 items-center py-2">
           <div className="flex flex-col">
             <p>neko</p>
