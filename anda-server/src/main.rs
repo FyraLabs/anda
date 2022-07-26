1d1dca1a4226da4f758785e86a6b460f549027a0
Cappy Ishihara <cappy@cappuchino.xyz>
2022-07-26 07:54:37 +0700

    use rust embed to embed folder

--- anda-server/src/main.rs
+++ anda-server/src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate rocket;
-use std::path::PathBuf;
+use std::{path::PathBuf, borrow::Cow, ffi::OsStr};
 
-use rocket::{Build, Rocket, fs::FileServer};
+use rocket::{Build, Rocket, fs::FileServer, response::content::RawHtml, http::ContentType};
 use sea_orm_rocket::Database;
 
 mod api;
@@ -14,11 +14,36 @@ mod entity;
 mod kubernetes;
 mod s3_object;
 
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "dist/"]
+struct Asset;
+
 #[get("/<_file..>")]
-async fn index(_file: PathBuf) -> std::io::Result<rocket::fs::NamedFile> {
-    rocket::fs::NamedFile::open(std::path::Path::new("dist").join("index.html")).await
+fn index(_file: PathBuf) -> Option<RawHtml<Cow<'static, [u8]>>> {
+  let asset = Asset::get("index.html")?;
+  Some(RawHtml(Cow::from(asset)))
 }
 
+#[get("/")]
+fn root() -> Option<RawHtml<Cow<'static, [u8]>>> {
+  let asset = Asset::get("index.html")?;
+  Some(RawHtml(Cow::from(asset)))
+}
+
+#[get("/<file..>" , rank = 10)]
+fn dist(file: PathBuf) -> Option<(ContentType, Cow<'static, [u8]>)> {
+    let filename = file.display().to_string();
+    let asset = Asset::get(&filename)?;
+    let content_type = file
+      .extension()
+      .and_then(OsStr::to_str)
+      .and_then(ContentType::from_extension)
+      .unwrap_or(ContentType::Bytes);
+
+    Some((content_type, Cow::from(asset)))
+}
 #[launch]
 async fn rocket() -> Rocket<Build> {
     rocket::build()
@@ -28,5 +53,5 @@ async fn rocket() -> Rocket<Build> {
         .mount("/projects", api::projects_routes())
         .mount("/app", routes![index])
         //.mount("/assets", FileServer::from("dist/assets"))
-        .mount("/", FileServer::from("dist"))
+        .mount("/", routes![dist, root])
 }
