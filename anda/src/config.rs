179e0f8762bb3b8151dbff62779caf3b5b252b2c
Cappy Ishihara <cappy@cappuchino.xyz>
2022-08-16 09:37:45 +0700

    add build mode config

--- anda/src/config.rs
+++ anda/src/config.rs
@@ -73,7 +73,26 @@ pub struct PostScript {
 #[derive(Deserialize, PartialEq, Eq)]
 pub struct RpmBuild {
     pub spec: PathBuf,
+    // serde default is standard
+    /// Mode to use for the build.
+    /// Default is `standard`. Builds an RPM normally from the spec file.
+    /// `cargo-rpm` builds uses the `cargo-rpm` crate to build an RPM from the Cargo.toml file, using templated values from the spec file.
+    #[serde(default = "default_rpm_mode")]
+    pub mode: RpmBuildMode
+
+
 }
+
+fn default_rpm_mode() -> RpmBuildMode {
+    RpmBuildMode::Standard
+}
+#[derive(Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+pub enum RpmBuildMode {
+    Standard,
+    CargoRpm,
+}
+
 
 #[derive(Deserialize, PartialEq, Eq)]
 pub struct Docker {
