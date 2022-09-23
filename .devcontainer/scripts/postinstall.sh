d518e61556c23119b7bc6bd478051c18f6ccd97b
Cappy Ishihara <cappy@cappuchino.xyz>
2022-09-23 09:43:12 +0000

    more flatpak boilerplate

--- .devcontainer/scripts/postinstall.sh
+++ .devcontainer/scripts/postinstall.sh
@@ -5,10 +5,10 @@ curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --profile c
 
 source "$HOME/.cargo/env"
 
-cargo install sea-orm-cli
+# cargo install sea-orm-cli
 
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# curl -fsSL https://get.pnpm.io/install.sh | sh -
+# curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 echo "export PNPM_HOME=\"/home/vscode/.local/share/pnpm\"" >> "$HOME/.zshrc"
 echo "export PATH=\"\$PNPM_HOME:$PATH\"" >> "$HOME/.zshrc"
@@ -20,22 +20,22 @@ sudo dnf install -y just nodejs dotnet-runtime-3.1 htop
 #sudo /usr/local/share/docker-init.sh
 sudo ln -sf /usr/libexec/docker/docker-proxy /usr/bin/docker-proxy
 
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
+# cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+# [kubernetes]
+# name=Kubernetes
+# baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+# enabled=1
+# gpgcheck=1
+# gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# EOF
+# sudo yum install -y kubectl
 
-wget https://github.com/moby/buildkit/releases/download/v0.10.3/buildkit-v0.10.3.linux-amd64.tar.gz -O /tmp/buildkit.tar.gz
+# wget https://github.com/moby/buildkit/releases/download/v0.10.3/buildkit-v0.10.3.linux-amd64.tar.gz -O /tmp/buildkit.tar.gz
 
-sudo tar -xzf /tmp/buildkit.tar.gz -C /usr/local/
+# sudo tar -xzf /tmp/buildkit.tar.gz -C /usr/local/
 
-wget https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-sudo mv mc /usr/local/bin/mc
+# wget https://dl.min.io/client/mc/release/linux-amd64/mc
+# chmod +x mc
+# sudo mv mc /usr/local/bin/mc
 
-echo "127.0.0.1 local-registry" | sudo tee -a /etc/hosts
+# echo "127.0.0.1 local-registry" | sudo tee -a /etc/hosts
--- anda/src/flatpak.rs
+++ anda/src/flatpak.rs
@@ -2,12 +2,48 @@ use std::path::PathBuf;
 
 use flatpak::{application::FlatpakApplication, format::FlatpakManifestFormat};
 
-
 pub struct FlatpakBuilder {
+    /// The output directory for the flatpak build
     output_dir: PathBuf,
+    /// The output flatpak repository
+    output_repo: PathBuf,
+    /// Extra sources as paths
+    extra_sources: Vec<PathBuf>,
+    /// Extra sources as URLs
+    extra_sources_urls: Vec<String>,
 }
 
-pub fn build_flatpak() {
-    let app = FlatpakApplication::parse(FlatpakManifestFormat::YAML, "");
+pub fn test_flatpak() {
+    // test code
 
+    // load file
+    let manifest = r###"
+    app-id: org.flatpak.Hello
+    runtime: org.freedesktop.Platform
+    runtime-version: '21.08'
+    sdk: org.freedesktop.Sdk
+    command: hello.sh
+    modules:
+      - name: hello
+        buildsystem: simple
+        build-commands:
+          - install -D hello.sh /app/bin/hello.sh
+        sources:
+          - type: file
+            path: hello.sh
+    "###;
+
+    println!("manifest: {:?}", manifest);
+    let app = FlatpakApplication::parse(FlatpakManifestFormat::YAML, manifest);
+    println!("app: {:#?}", app);
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    #[test]
+    fn a() {
+        test_flatpak();
+    }
 }
