0ce468393b133f8159007b4e468acae57d675321
madonuko <madonuko@outlook.com>
2024-07-16 19:21:42 +0800

    feat(update): script stats (time taken to run)

--- src/update.rs
+++ src/update.rs
@@ -1,6 +1,7 @@
 use anda_config::Manifest;
 use andax::{run, RPMSpec};
 use color_eyre::Result;
+use std::io::Write;
 use std::{
     collections::BTreeMap,
     thread::{self, Builder},
@@ -14,6 +15,7 @@ pub fn update(
     fls: BTreeMap<String, String>,
 ) -> Result<()> {
     let mut handlers = vec![];
+    let project_count = cfg.project.len();
     'p: for (name, mut proj) in cfg.project {
         if let Some(scr) = &proj.update {
             trace!(name, scr = scr.to_str(), "Th start");
@@ -28,10 +30,12 @@ pub fn update(
                 continue 'p; // for any filters !match labels in proj (strict)
             }
             let fls = fls.clone();
-            handlers.push(Builder::new().name(name).spawn(move || {
+            let alias = proj.alias.into_iter().flatten().next().clone().unwrap_or(name);
+            handlers.push(Builder::new().name(alias).spawn(move || {
                 let th = thread::current();
                 let name = th.name().expect("No name for andax thread??");
                 let scr = proj.update.expect("No update script? How did I get here??");
+                let start = std::time::SystemTime::now();
                 let sc = run(name, &scr, lbls, |sc| {
                     // we have to do it here as `Dynamic` in andax::Map nu Sync impl
                     let mut filters = andax::Map::new();
@@ -43,24 +47,47 @@ pub fn update(
                         sc.push("rpm", RPMSpec::new(name.to_string(), &scr, &rpm.spec));
                     }
                 });
+                let duration = std::time::SystemTime::now().duration_since(start).unwrap();
                 if let Some(sc) = sc {
                     let rpm: RPMSpec = sc.get_value("rpm").expect("No rpm object in rhai scope");
                     if let Err(e) = rpm.write() {
                         error!("{name}: Failed to write RPM: {e}");
                     }
                 }
+                duration
             })?);
         }
     }
 
     debug!("Joining {} threads", handlers.len());
+    let mut tasks = vec![];
+    let handlers_count = handlers.len();
 
     for hdl in handlers {
         let th = hdl.thread();
         let name = th.name().expect("No name for andax thread??").to_string();
-        if let Err(e) = hdl.join() {
-            error!("Panic @ `{name}` : {e:#?}");
+        match hdl.join() {
+            Ok(duration) => tasks.push((name, duration)),
+            Err(e) => error!("Panic @ `{name}` : {e:#?}"),
         }
+    }
+
+    tasks.sort_unstable_by(|(_, duration0), (_, duration1)| duration1.cmp(duration0));
+    let mut stdout = std::io::stdout();
+
+    writeln!(
+        stdout,
+        "\nFinished running {}/{project_count} tasks, {} failed fatally.",
+        tasks.len(),
+        handlers_count - tasks.len()
+    )
+    .unwrap();
+    writeln!(stdout, "Here is a list of unfiltered tasks:\n").unwrap();
+    writeln!(stdout, "      Time (ms)   Project").unwrap();
+    writeln!(stdout, "No.   ══════════╤═════════").unwrap();
+
+    for (n, (name, duration)) in tasks.into_iter().enumerate() {
+        writeln!(stdout, "{:<5} {:>9} │ {name}", n + 1, duration.as_millis()).unwrap();
     }
 
     Ok(())
