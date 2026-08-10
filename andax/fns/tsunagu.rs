6c53f091aeb307ef1396ab5ced7f7ccb7c588245
Jan200101 <sentrycraft123@gmail.com>
2026-08-10 18:19:46 +0200

    feat(andax): add git_tags function

--- andax/fns/tsunagu.rs
+++ andax/fns/tsunagu.rs
@@ -1,4 +1,5 @@
 use crate::{error::AndaxRes, run::rf};
+use core::str::FromStr;
 use git2::Remote;
 use rhai::{
     plugin::{export_module, Dynamic, EvalAltResult, NativeCallContext},
@@ -611,6 +612,45 @@ pub mod ar {
         let v: Value = req.call().ehdl(&ctx)?.into_body().read_json().ehdl(&ctx)?;
         trace!("Got json from {repo} hosted with Forgejo:\n{v}");
         Ok(v[0]["sha"].as_str().unwrap_or("").to_owned())
+    }
+
+    #[rhai_fn(return_raw, global)]
+    pub fn git_tags(ctx: NativeCallContext, url: &str) -> Res<rhai::Array> {
+        const TAGS_PREFIX: &str = "refs/tags/";
+        const RESOLVED_SUFFIX: &str = "^{}";
+
+        let mut tags = rhai::Array::new();
+
+        let req = (AGENT.get(&format!("{url}/info/refs?service=git-upload-pack")))
+            .header("Content-Type", "application/x-git-upload-pack-request");
+
+        let resp = req.call().ehdl(&ctx)?;
+        let is_smart = resp.headers().iter().any(|(key, value)| {
+            key == "Content-Type" && value == "application/x-git-upload-pack-advertisement"
+        });
+        let body: String = resp.into_body().read_to_string().ehdl(&ctx)?;
+
+        for line in body.lines().map(str::trim).filter(|a| !a.is_empty() && !a.starts_with('#')) {
+            let name = if is_smart {
+                let line_length = usize::from_str_radix(&line[..4], 16).unwrap_or(0).checked_sub(1);
+                let Some(line_length) = line_length else {
+                    continue;
+                };
+                line[4..line_length].split_once(' ').map_or("", |(_ref, name)| name)
+            } else {
+                line.split_once('\t').map_or("", |(_ref, name)| name)
+            };
+
+            if !name.starts_with(TAGS_PREFIX) || name.ends_with(RESOLVED_SUFFIX) {
+                continue;
+            }
+
+            if let Ok(tag) = rhai::Dynamic::from_str(&name[TAGS_PREFIX.len()..]) {
+                tags.push(tag);
+            }
+        }
+
+        Ok(tags)
     }
 
     #[rhai_fn(skip)]
